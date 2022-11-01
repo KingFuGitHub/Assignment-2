b4dbc9672e43aaae1b31fb92ef83160fe9de9032
Long Nguyen <kingfu321@gmail.com>
2022-10-31 22:51:51 -0700

    1st commit from Mac

--- Mini Twitter/TreeView.java
+++ Mini Twitter/TreeView.java
@@ -1,6 +1,6 @@
 public class TreeView{
 
     public static void main(String[] args) {
-        System.out.println("This is assignment 2");
+        System.out.println("This is assignment 2 (from macbook air)");
     }
 }
