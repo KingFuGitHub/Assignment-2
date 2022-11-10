ecc958359c858f1a8e4b2f623c326cf333f9a59d
Long Nguyen <kingfu321@gmail.com>
2022-11-10 01:59:37 -0800

    14th commit

--- Mini Twitter/UserPanel.java
+++ Mini Twitter/UserPanel.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.DefaultListModel;
@@ -10,28 +11,25 @@ import javax.swing.JTextField;
 
 public class UserPanel extends JFrame {
 
-    private User currentUserInfo;
-
     public void userPanel(Object nodeInfo, HashMap<String, User> userData) {
-        // private User test = user;
 
         String week[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturaday", "Sunday" };
 
         if (nodeInfo != null && nodeInfo instanceof User) {
-            currentUserInfo = userData.get(nodeInfo.toString());
-            String currentUserInfoString = currentUserInfo.toString();
+            User currentUserInfo = userData.get(nodeInfo.toString());
+            // String week1[] = currentUserInfo.getFollowing();
 
             JFrame userPanelFrame = new JFrame(currentUserInfo.getID());
             JButton followUserButton = new JButton("Follow User");
             JTextField userIDTextField = new JTextField();
             userIDTextField.setHorizontalAlignment(JTextField.CENTER);
             // JList followerList = new JList(user.getFollowing().toArray());
-            JList followerList = new JList(currentUserInfo.getFollowing().toArray());
+            JList followerList = new JList<>(currentUserInfo.getFollowing().toArray());
             followerList.setSelectedIndex(2);
             JTextArea tweetMessage = new JTextArea();
             JScrollPane tweetMessageScrollPane = new JScrollPane(tweetMessage);
             JButton postTweetButton = new JButton("Post Tweet");
-            JList newsFeedList = new JList(week);
+            JList newsFeedList = new JList<>(week);
             newsFeedList.setSelectedIndex(3);
 
             userPanelFrame.setSize(350, 500);
@@ -53,33 +51,39 @@ public class UserPanel extends JFrame {
             userPanelFrame.add(postTweetButton);
             userPanelFrame.add(newsFeedList);
 
-            userPanelFrame.invalidate();
-            userPanelFrame.validate();
-            userPanelFrame.repaint();
+            // userPanelFrame.invalidate();
+            // userPanelFrame.validate();
+            // userPanelFrame.repaint();
 
+            // public String userID1 = userIDTextField.getText().toString().toLowerCase();
 
             followUserButton.addActionListener(e -> {
-                String userID = userIDTextField.getText().toString().toLowerCase();
-                if(userID != ""){
-                    System.out.println("1. true: " + userID);
-                }
+                String userID = userIDTextField.getText();
 
-                if(currentUserInfo.isContainID(userID) == false){
-                    System.out.println("2. true: " + currentUserInfo.isContainID(userID));
-                }
-
-                if(currentUserInfoString != userID){
-                    System.out.println("3. true: " + currentUserInfo.getID() + " | " + userID);
-
-                }
-                if (userID != "" && currentUserInfo.isContainID(userID) == false && currentUserInfo.getID().toString() != userID) {
+                // System.out.println("1. " + userID);
+                // System.out.println("2. " + currentUserInfo.isContainID(userID));
+                // System.out.println("3. " +
+                // currentUserInfo.isContainID(currentUserInfo.getID()));
+                // System.out.println("4. " + currentUserInfo.isContainID(nodeInfo.toString()));
+                // System.out.println("5. " + currentUserInfo.getID());
+                // System.out.println("6. " + nodeInfo.toString());
+                System.out.println("Size: " + currentUserInfo.getFollowing().size());
+                    System.out.println(nodeInfo.toString().toLowerCase());
+                    System.out.println(currentUserInfo.getID().toString().toLowerCase());
+                    System.out.println(userID);
+                if (userID != "" && currentUserInfo.isContainID(userID) == false && nodeInfo.toString().toLowerCase() != userIDTextField.getText()) {
                     currentUserInfo.setFollowing(userID);
+
                     followerList.setListData(currentUserInfo.getFollowing().toArray());
 
-                    // followerList.invalidate();
-                    // followerList.validate();
-                    // followerList.repaint();
+                    System.out.println("executed");
+                    followerList.invalidate();
+                    followerList.validate();
+                    followerList.repaint();
                 }
+                followerList.invalidate();
+                followerList.validate();
+                followerList.repaint();
                 userPanelFrame.invalidate();
                 userPanelFrame.validate();
                 userPanelFrame.repaint();
